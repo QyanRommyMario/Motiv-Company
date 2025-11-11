c6aaa5cdb648bcea01348318c56aab9d1be8c3ac
Qyan Rommy Mario <149346772+QyanRommyMario@users.noreply.github.com>
2025-11-11 19:58:14 +0700

    chore: remove temporary debugging files and exposed secrets

--- check-b2brequest.sql
+++
@@ -1,5 +0,0 @@
--- Check B2BRequest columns
-SELECT column_name, data_type, is_nullable, column_default
-FROM information_schema.columns 
-WHERE table_name = 'B2BRequest'
-ORDER BY ordinal_position;
