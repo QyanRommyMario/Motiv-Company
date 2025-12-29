52a9c837bb6b5e5d0b5728ba77292bdcb9775906
Qyan Rommy Mario <149346772+QyanRommyMario@users.noreply.github.com>
2025-12-29 10:31:56 +0700

    chore: remove unused Prisma files and scripts (migrated to Supabase)

--- prisma/migrations/20251102084959_add_country_field/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN     "shippingCountry" TEXT NOT NULL DEFAULT 'Indonesia';
-
--- AlterTable
-ALTER TABLE "ShippingAddress" ADD COLUMN     "country" TEXT NOT NULL DEFAULT 'Indonesia';
--- prisma/migrations/20251107043552_add_product_features/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "features" TEXT[] DEFAULT ARRAY[]::TEXT[];
